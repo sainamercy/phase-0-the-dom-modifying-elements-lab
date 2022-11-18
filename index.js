eab954ebed6d35afa37e20dcbbc0fc2d8eced747
sainamercy <sainamercy@gmail.com>
2022-11-18 08:09:46 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.querySelector("#main")
+
+main.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.innerHTML ="Mercy is the champion"
+
+document.body.prepend(newHeader)
